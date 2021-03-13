29e77fec4aae310a7dba85f68982b0908d27e0e0
Beyond <1252616362@qq.com>
2021-03-13 15:15:44 +0800

    add presentProject field in MainActivity DataFragment will read presentProject when load

--- app/src/main/java/com/beyond/beidou/data/DataHomeFragment.java
+++ app/src/main/java/com/beyond/beidou/data/DataHomeFragment.java
@@ -106,6 +106,7 @@ public class DataHomeFragment extends BaseFragment {
                         }
 
 
+
                         spProjectName.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                             @Override
                             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
