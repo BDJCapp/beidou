636a053c84c13f9e0ed611887e46a2e2d660cdf9
Beyond-Ly <1252616362@qq.com>
2021-12-11 13:38:51 +0800

    添加估计曲线及DE、DN、DH

--- app/src/main/java/com/beyond/beidou/data/ChartFragment.java
+++ app/src/main/java/com/beyond/beidou/data/ChartFragment.java
@@ -130,7 +130,6 @@ public class ChartFragment extends BaseFragment implements View.OnClickListener 
         }
     };
 
-
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
